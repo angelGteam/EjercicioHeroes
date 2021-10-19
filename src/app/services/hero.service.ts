bc6b666029f4f3fd7d0c9611dd7fc46d72cac643
Angel Gomez <agomez@gteam.es>
2021-10-19 22:44:01 +0200

    Add a new hero

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -63,4 +63,11 @@ export class HeroService {
       return of(result as T);
     };
   }
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 }
