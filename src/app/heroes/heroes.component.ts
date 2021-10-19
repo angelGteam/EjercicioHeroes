a04b684ef99ad757cb2da3b2b89a0cd67dc43842
Angel Gomez <agomez@gteam.es>
2021-10-19 21:37:39 +0200

    paso 5 completado

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../services/hero.service';
+import { MessageService } from '../services/message.service';
+import { Hero } from '../types/hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+}
+
